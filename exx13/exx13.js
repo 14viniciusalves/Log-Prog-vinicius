67212ced9468507dec93d9681b6a71798d53b5c9
14viniciusalves <14viniciusalvescarvalho@gmail.com>
2026-05-26 21:42:24 -0300

    04

--- exx13/exx13.js
+++ exx13/exx13.js
@@ -1,1 +1,6 @@
+var num1 = document.getElementById(number1)
+var num2 = document.getElementById(number2)
+var num3 = document.getElementById(number3)
+var result = document.getElementById("")
 
+if(num1  >=)
