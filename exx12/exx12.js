1cc9c1c1a67b4ca21ed66274f34cf5c22a2f7b02
14viniciusalves <14viniciusalvescarvalho@gmail.com>
2026-05-26 21:13:07 -0300

    aula04

--- exx12/exx12.js
+++ exx12/exx12.js
@@ -1,12 +1,12 @@
+function calcular(){
+
+
 var nome = window.prompt(`ME FALE SEU NOME:`)
 var nota1 = Number.parseInt(window.prompt(`ME FALA SUA PRIMEIRA NOTA ${nome}:`))
 var nota2 = Number.parseInt(window.prompt(`ME FALE SUA SEGUNDA NOTA ${nome}:`))
 
-var soma = nota1 + nota2 /2
-
-
-if()
-
+var soma = (nota1 + nota2) /2
+var result = ""
 
 
 
@@ -14,3 +14,4 @@ if()
 
 
 
+}
