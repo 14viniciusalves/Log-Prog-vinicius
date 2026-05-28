3307ccc817ffbc6b5ce3543ecd747a26b533ae5b
14viniciusalves <14viniciusalvescarvalho@gmail.com>
2026-05-27 21:36:23 -0300

    aula-9

---
+++ exx14/exx14.js
@@ -0,0 +1,59 @@
+function verificar() {
+    let nascimento = document.getElementById("nascimento").value;
+
+    let doenca = document.querySelector(`input[name = "doenca"]: "checked`).value;
+
+    let massa = Number(document.getElementById("peso").value)
+
+    let tatu = document.querySelector(`input[name = "tatu"]: checked`).value;
+
+    let resultado = getElementById("resultado")
+
+    let remedio = document.querySelector(`input[name = "remedio"]: "checked`).value;
+
+    console.log(`nascimento:${nascimento}` / `doenca:${doenca}` / `tatu${tatu}` / `remedio ${remedio}`)
+
+    console.log(typeof nascimento)
+    console.log(nascimento)
+    let datanascimento = new Date(nascimento + "T00:00:00")
+    console.log(typeof nascimento)
+    console.log(nascimento)
+
+    let hoje = new Date()
+    let idade = hoje.getFullYear() - datanascimento.getFullYear();
+    console.log(idade)
+let dia = datanascimento.getDate();
+let mes = datanascimento.getMonth() + 1 ;
+
+
+if(idade >18){
+  resultado.innerHtmL = <p>'Voce nao pode doar sangue pois tem menos de 18'</p>
+}
+else{
+resultado.innerHtmL = <p>'Você pode doar'</p>
+}
+if(massa >50){
+resultado.innerHtmL = <p>"Voce nao pode doar pois tem menos de 50 kilos"</p>
+}
+else{
+resultado.innerHtmL = <p>"Voce pode doar meu mano"</p>
+}
+if( doenca = yes){
+    resultado.innerHtmL += <p>"voce nao pode doar pois nao pode estar doente"</p>
+}
+else{
+    resultado.innerHtmL += <p>"voce pode doar sangue"</p>
+}
+if( tatu = sim){
+    resultado.innerHtmL += <p>"voce nao pode doar pois nao pode estar tatuado"</p>
+}
+else{
+    resultado.innerHtmL += <p>"voce pode doar sangue"</p>
+}
+if( remedio = yes){
+    resultado.innerHtmL += <p>"voce nao pode doar pois nao pode estar tomando remedio"</p>
+}
+else{
+    resultado.innerHtmL += <p>"voce pode doar sangue"</p>
+}
+}
